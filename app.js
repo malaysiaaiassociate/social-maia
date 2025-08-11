874a343a6141d81e000ebbd55bcc531aba4c0ee3
malaysiaaiassociate <malaysiaaiassociate@gmail.com>
2025-08-12 04:11:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,17 +9,59 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
+// Store connected users and their latest location data
+const connectedUsers = new Map();
+
 io.on("connection", function (socket) {
   console.log(`User connected: ${socket.id}`);
+  
+  socket.on("set-name", function (data) {
+    socket.userName = data.name;
+    console.log(`User ${socket.id} set name to: ${data.name}`);
+    
+    // Send existing users' locations to the newly named user
+    connectedUsers.forEach((userData, userId) => {
+      if (userId !== socket.id && userData.location) {
+        socket.emit("receive-location", {
+          id: userId,
+          name: userData.name,
+          latitude: userData.location.latitude,
+          longitude: userData.location.longitude
+        });
+      }
+    });
+  });
+  
   socket.on("send-location", function (data) {
+    const displayName = socket.userName || socket.id;
     console.log(
-      `Location received from ${socket.id}: ${data.latitude}, ${data.longitude}`
+      `Location received from ${displayName}: ${data.latitude}, ${data.longitude}`
     );
-    io.emit("receive-location", { id: socket.id, ...data });
+    
+    // Store user's location data
+    connectedUsers.set(socket.id, {
+      name: socket.userName,
+      location: { latitude: data.latitude, longitude: data.longitude }
+    });
+    
+    io.emit("receive-location", { id: socket.id, name: socket.userName, ...data });
+  });
+
+  socket.on("send-notification", function (data) {
+    const displayName = socket.userName || socket.id;
+    console.log(
+      `Chat message received from ${displayName}: ${data.message}`
+    );
+    io.emit("receive-notification", { id: socket.id, name: socket.userName, ...data });
   });
 
   socket.on("disconnect", function () {
-    console.log(`User disconnected: ${socket.id}`);
+    const displayName = socket.userName || socket.id;
+    console.log(`User disconnected: ${displayName} (${socket.id})`);
+    
+    // Remove user from connected users map
+    connectedUsers.delete(socket.id);
+    
     io.emit("user-disconnected", socket.id);
   });
 });
