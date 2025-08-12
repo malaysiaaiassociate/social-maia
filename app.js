a399a5357a00b2d4df487cf9ab32de6519e4b65b
malaysiaaiassociate <malaysiaaiassociate@gmail.com>
2025-08-12 12:36:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -70,6 +70,7 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
